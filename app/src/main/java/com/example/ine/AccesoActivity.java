258098c35118f7fda813c61287ab4f052fd23fba
Arturo Díaz <l18a4d97h@hotmail.com>
2019-11-28 09:38:23 -0600

    Avance de proyecto 2

--- app/src/main/java/com/example/ine/AccesoActivity.java
+++ app/src/main/java/com/example/ine/AccesoActivity.java
@@ -2,13 +2,73 @@ package com.example.ine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AccesoActivity extends AppCompatActivity {
+
+    public TextInputLayout editUsuario, editContrasenia;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceso);
+
+        editUsuario=findViewById(R.id.AccesoET1);
+        editContrasenia=findViewById(R.id.AccesoET2);
+        Button bIngresar=findViewById(R.id.AccesoBT1);
+        bIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ingresar(view);
+            }
+        });
+
+        Button bSalir = findViewById(R.id.AccesoBT2);
+        bSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
+        progressDialog = new ProgressDialog(this);
+
     }
+
+    public void ingresar(View view) {
+        String nombre = editUsuario.getEditText().getText().toString();
+        String contrasenia = editContrasenia.getEditText().getText().toString();
+        if (TextUtils.isEmpty(nombre)) {
+            Toast.makeText(this, "Se debe ingresar un Usuario", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (TextUtils.isEmpty(contrasenia)) {
+            Toast.makeText(this, "Se debe ingresar una Contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            //progressDialog.setMessage("Iniciando sesion...");
+            //progressDialog.show();
+            if (nombre.equals("Arturo") && contrasenia.equals("CM19")) {
+                Toast.makeText(getApplicationContext(), "Sesion Iniciada", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(AccesoActivity.this,
+                        MainActivity.class);
+                myintent.putExtra("Nombre", editUsuario.toString());
+                startActivity(myintent);
+                //progressDialog.dismiss();
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos: " + nombre + " " + contrasenia, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/ine/firebase/Activos.java
@@ -0,0 +1,122 @@
+package com.example.ine.firebase;
+
+public class Activos {
+
+    String Nombre;
+    String Domicilio;
+    String FechaNacimiento;
+    String Genero;
+    String ClaveElectoral;
+    String Curp;
+    String AnioRegistro;
+    String NumeroVersion;
+    String Distrito;
+    String Seccion;
+    String Localidad;
+    String AnioEmision;
+    String Vigencia;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return Domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        Domicilio = domicilio;
+    }
+
+    public String getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        FechaNacimiento = fechaNacimiento;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String genero) {
+        Genero = genero;
+    }
+
+    public String getClaveElectoral() {
+        return ClaveElectoral;
+    }
+
+    public void setClaveElectoral(String claveElectoral) {
+        ClaveElectoral = claveElectoral;
+    }
+
+    public String getCurp() {
+        return Curp;
+    }
+
+    public void setCurp(String curp) {
+        Curp = curp;
+    }
+
+    public String getAnioRegistro() {
+        return AnioRegistro;
+    }
+
+    public void setAnioRegistro(String anioRegistro) {
+        AnioRegistro = anioRegistro;
+    }
+
+    public String getNumeroVersion() {
+        return NumeroVersion;
+    }
+
+    public void setNumeroVersion(String numeroVersion) {
+        NumeroVersion = numeroVersion;
+    }
+
+    public String getDistrito() {
+        return Distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        Distrito = distrito;
+    }
+
+    public String getSeccion() {
+        return Seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        Seccion = seccion;
+    }
+
+    public String getLocalidad() {
+        return Localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        Localidad = localidad;
+    }
+
+    public String getAnioEmision() {
+        return AnioEmision;
+    }
+
+    public void setAnioEmision(String anioEmision) {
+        AnioEmision = anioEmision;
+    }
+
+    public String getVigencia() {
+        return Vigencia;
+    }
+
+    public void setVigencia(String vigencia) {
+        Vigencia = vigencia;
+    }
+}
